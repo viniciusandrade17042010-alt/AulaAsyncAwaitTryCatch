0f731506462d7d4fdb44fd96649cf05239062e0c
VINICIUS ANDRADE OLIVEIRA <26171794@edu119.local>
2026-09-03 11:53:42 -0300

    subindo projeto

---
+++ 01 sicrono.js
@@ -0,0 +1,5 @@
+//O JavaScript executa uma instrução depois da outra
+
+console.log("1-Início");
+console.log("2-Processando...");
+console.log("3-Fim");
---
+++ 02 setTimeout.js
@@ -0,0 +1,7 @@
+console.log(" Pedido Realizado");
+
+setTimeout(() => {
+    console.log("Pedido ficou pronto");
+},3000);
+
+console.log("3 - CLiente pode continuar aguardando")
